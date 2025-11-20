c1d20bfd3f1e6d3918279aa5bf3811b97e3ca03c
Andre Dwiyanto C <andredc@MacBook-Pro-5.local>
2025-11-21 01:06:27 +0700

    git push origin main --force Hapus semua file kecuali README.md

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    session_unset();
-    session_destroy();
-        echo "<script>alert('Anda telah keluar');</script>";
-        echo "<script>location='login.php';</script>";
-?>
